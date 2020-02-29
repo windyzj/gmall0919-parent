5aa12225cb4e7f4965c1a16e51a5f54bdf571e3f
zhangchen <windyzj@163.com>
2020-02-29 16:57:11 +0800

    首次上传

---
+++ gmall0919-logger/src/main/java/com/atguigu/gmall0919/logger/Gmall0919LoggerApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall0919.logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gmall0919LoggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gmall0919LoggerApplication.class, args);
+    }
+
+}
---
+++ gmall0919-logger/src/main/java/com/atguigu/gmall0919/logger/controller/LoggerController.java
@@ -0,0 +1,41 @@
+package com.atguigu.gmall0919.logger.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.atguigu.gmall0919.common.constant.GmallConstant;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController  //ResponseBody+Controller
+@Slf4j
+public class LoggerController {
+
+    @Autowired
+    KafkaTemplate kafkaTemplate;
+
+    //@RequestMapping(value = "log" ,method = RequestMethod.POST)
+    @PostMapping("log")
+    public String dolog(@RequestParam("logString") String logString ){
+        System.out.println(logString);
+
+        JSONObject jsonObject = JSON.parseObject(logString);
+        jsonObject.put("ts",System.currentTimeMillis());
+        String jsonString = jsonObject.toJSONString();
+        //落盘日志  log4j logback log4j2  loging   -->>  slf4j
+        log.info(jsonString);
+        // 发送kafka
+        if(jsonObject.getString("type").equals("startup")){
+            kafkaTemplate.send(GmallConstant.KAFKA_TOPIC_STARTUP,jsonString);
+        }else{
+            kafkaTemplate.send(GmallConstant.KAFKA_TOPIC_EVENT,jsonString);
+        }
+
+
+        return  "success";
+
+    }
+
+}
